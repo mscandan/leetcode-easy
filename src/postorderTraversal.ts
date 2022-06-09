cd32637e694d57e6d9e231d2e54fb83a4213be7e
M. Selcuk Candan <mehmetselcukcandan@icloud.com>
2022-06-09 18:38:11 +0300

    add solution for postorder traversal

---
+++ src/postorderTraversal.ts
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+class TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+  }
+}
+
+function postorderTraversal(root: TreeNode | null): number[] {
+  const values: Array<number> = [];
+  if (!root) return values;
+  let stack = [root];
+  while (stack.length) {
+    let curr = stack.pop();
+    if (curr) {
+      if (curr.left) stack.push(curr.left);
+      if (curr.right) stack.push(curr.right);
+      values.unshift(curr.val);
+    }
+  }
+
+  return values;
+}
